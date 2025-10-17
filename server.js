d574a6fb1f32ca29e9add340d26623575d0a0c60
Camden Montgomery <camden.montgomery@sargas.ai>
2025-10-16 23:12:45 -0400

    Modified server.js to match closer to vercel + express examples

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const express = require('express')
-const { readFile, createWriteStream } = require('fs')
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const app = express()
 
-const pureImage = require('pureimage')
-
-//Need to 'serve' the static files (The JS, Images, & CSS)
-app.use(express.static('./public'))
 
 
 //Respond To Get Request At Base URL '/'
 app.get('/*',  async ( request, response ) => { //So Adding The Asterisk Does Indeed Make It Accept Any Text That Gets Passed Into The URL
-  response.sendFile('./public/index.html')
+  response.sendFile(path.join(__dirname,  'public', 'index.html'))
 })
 
-// listen for requests
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+export default app
